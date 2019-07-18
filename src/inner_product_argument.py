2a620f56e3c5e1d4deb0e1660f6b951ae93f67a2
William Borgeaud <williamborgeaud@gmail.com>
2019-07-19 00:04:29 +0200

    works before pippengen

--- src/inner_product_argument.py
+++ src/inner_product_argument.py
@@ -17,7 +17,7 @@ class Verifier:
 
 class NIProver:
 
-    def __init__(self, g, h, u, P, c, a, b, group):
+    def __init__(self, g, h, u, P, c, a, b, group, fast):
         assert len(g) == len(h) == len(a) == len(b)
         self.g = g
         self.h = h
@@ -28,6 +28,7 @@ class NIProver:
         self.b = b
         self.group = group
         self.transcript = b''
+        self.fast = fast 
     
     def prove(self):
         self.transcript += (
@@ -40,7 +41,11 @@ class NIProver:
         x = mod_hash(self.transcript, self.group.order)
         self.transcript += str(x).encode()
         P_new = self.P + (x*self.c) * u
-        Prov2 = FastNIProver2(self.g, self.h, x*self.u, P_new, self.a, self.b, self.group, self.transcript)
+        if self.fast:
+            Prov2 = FastNIProver2(self.g, self.h, x*self.u, P_new, self.a, self.b, self.group, self.transcript)
+        else:
+            Prov2 = NIProver2(self.g, self.h, x*self.u, P_new, self.a, self.b, self.group, self.transcript)
+
         return Prov2.prove()
 
 class NIProver2:
@@ -66,6 +71,7 @@ class NIProver2:
         R = vector_commitment(self.g[:np],self.h[np:],self.a[np:],self.b[:np]) + cr*self.u
         self.transcript += (point_to_bytes(L) + point_to_bytes(R))
         x = mod_hash(self.transcript, self.group.order)
+        # x = ModP(len(self.a)+1, self.group.order)
         self.transcript += str(x).encode()
         gp = [x.inv()*gi_fh + x*gi_sh for gi_fh,gi_sh in zip(self.g[:np],self.g[np:])]
         hp = [x*hi_fh + x.inv()*hi_sh for hi_fh,hi_sh in zip(self.h[:np],self.h[np:])]
@@ -95,9 +101,13 @@ class FastNIProver2:
         ss = []
         for i in range(1, self.n+1):
             tmp = ModP(1, self.group.order)
-            for j in range(1, self.log_n+1):
-                b = 1 if bin(i-1)[2:].zfill(self.log_n+1)[-j]=='1' else -1 
-                tmp *= xs[j-1] if b==1 else xs[j-1].inv()
+            # for j in range(1, self.log_n+1):
+            #     b = 1 if bin(i-1)[2:].zfill(self.log_n+1)[-j]=='1' else -1 
+            #     print(i,j,b)
+            #     tmp *= xs[j-1] if b==1 else xs[j-1].inv()
+            for j in range(0, self.log_n):
+                b = 1 if bin(i-1)[2:].zfill(self.log_n)[j]=='1' else -1 
+                tmp *= xs[j] if b==1 else xs[j].inv()
             ss.append(tmp)
         return ss
 
@@ -115,7 +125,7 @@ class FastNIProver2:
         while True:
             if len(ap) == len(bp) == len(gp) == len(hp) == 1:
                 ss = self.get_ss(xs)
-                return self.g, self.h, self.u, self.P, ap, bp, xs, ss, Ls, Rs, self.transcript
+                return self.u, self.P, ap, bp, xs, ss, Ls, Rs, self.transcript
             np = len(ap) // 2
             cl = inner_product(ap[:np],bp[np:])
             cr = inner_product(ap[np:],bp[:np])
@@ -125,6 +135,7 @@ class FastNIProver2:
             Rs.append(R)
             self.transcript += (point_to_bytes(L) + point_to_bytes(R))
             x = mod_hash(self.transcript, self.group.order)
+            # x = ModP(len(ap)+1, self.group.order)
             xs.append(x)
             self.transcript += str(x).encode()
             gp = [x.inv()*gi_fh + x*gi_sh for gi_fh,gi_sh in zip(gp[:np],gp[np:])]
@@ -138,11 +149,30 @@ def verify(g,h,u,P,a,b):
         print('OK')
     else:
         print('Not OK')
+    return P, a*g + b*h + c*u
+
+def fast_verify(g, h, u, P, a, b, xs, ss, Ls, Rs, transcript):
+    LHS = (a*b) * u
+    for i,gi in enumerate(g):
+        LHS += (a*ss[i]) * gi
+    for i,hi in enumerate(h):
+        LHS += (b*ss[i].inv()) * hi
+
+    RHS = P
+    for xi, Li, Ri in zip(xs, Ls, Rs):
+        RHS += (xi**2) * Li
+        RHS += (xi.inv()**2) * Ri
+    
+    if LHS == RHS:
+        print('OK')
+    else:
+        print('Not OK')
+
 
 CURVE = SECP256k1.curve
 p = SECP256k1.order
 
-N = 16
+N = 128
 
 g = [elliptic_hash(str(i).encode() + b'This', CURVE) for i in range(N)]
 h = [elliptic_hash(str(i).encode() + b'is', CURVE) for i in range(N)]
@@ -153,12 +183,18 @@ b = [mod_hash(str(i).encode() + b'still testing', p) for i in range(N)]
 
 P = vector_commitment(g,h,a,b)
 c = inner_product(a,b)
-# self.g, self.h, self.u, self.P, ap, bp, xs, ss, Ls, Rs self.transcript
 
-Prov = NIProver(g,h,u,P,c,a,b,SECP256k1)
+Prov = NIProver(g,h,u,P,c,a,b,SECP256k1,False)
 pg, ph, pu, pP, pa, pb, pTranscript = Prov.prove()
 
-verify(pg[0],ph[0],pu,pP,pa[0],pb[0])
-print(pTranscript)
-print(len(pTranscript))
-print(len(hex(int(pTranscript))))
+tLHS, tRHS = verify(pg[0],ph[0],pu,pP,pa[0],pb[0])
+
+Prov = NIProver(g,h,u,P,c,a,b,SECP256k1,True)
+# pg, ph, pu, pP, pa, pb, pTranscript = Prov.prove()
+pu, pP, pa, pb, pxs, pss, pLs, pRs, pTranscript = Prov.prove()
+
+# verify(pg[0],ph[0],pu,pP,pa[0],pb[0])
+fast_verify(g,h,pu,pP,pa[0],pb[0],pxs,pss,pLs,pRs,pTranscript)
+# print(pTranscript)
+# print(len(pTranscript))
+# print(len(hex(int(pTranscript))))
